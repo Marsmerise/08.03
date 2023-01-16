cb722094be98e0b44da5bbb4e1be44a7dd8cad29
Carlo Pauly <carlo.pauly@hotmail.de>
2023-01-16 14:44:20 +0100

    uploaded files

---
+++ namen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	int i, y; 
+	int anzahlUnterstriche;
+
+	char* vornamen[5] = {"Peter", "Hans", "Wolfgang", "Donald", "Waldtraud"};
+	char* nachnamen[5] = {"Mueller", "Schmidt", "Meier", "Duck", "Schulz"};
+	
+	for (i = 0; i < 5; i++) {
+
+		anzahlUnterstriche = 81 - strlen (vornamen[i]) - strlen (nachnamen[i]);
+		char us[anzahlUnterstriche];
+	
+		for (y = 0; y <= anzahlUnterstriche; y++) {
+
+			us[y] = '_';
+
+		}	
+	
+		printf("%-s %s %s\n", vornamen[i], us, nachnamen[i]);
+	
+	}
+
+return 0;
+
+}
